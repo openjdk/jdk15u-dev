50cb043fccf648b44e36f7d5db610c6ab2aaaffb
Sergey Bylokhov <bylokhov@amazon.com>
2022-02-12 21:24:22 -0800

    Restore the "import java.util.stream.Collectors;"

--- test/jdk/build/AbsPathsInImage.java
+++ test/jdk/build/AbsPathsInImage.java
@@ -32,6 +32,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
